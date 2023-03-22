7cc288222ab2aad5a7a0e715b0e7db5da475f34a
robson-rsp <126628714+robson-rsp@users.noreply.github.com>
2023-03-22 12:08:05 -0300

    app - editado

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def prediction(model, year, price, transmission, mileage, fueltype, tax, mpg, en
     print(X)
     print(str(type(model)))
     #return model.predict(X)
-    return str(type(pipeline))
+    return str(X))
 
 
 
